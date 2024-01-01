94d2f977fceabf0cb745ee68466d9cb16fd784cb
GetRusty <getrusty@kaist.ac.kr>
2024-01-02 03:42:42 +0900

    Removed unused imports

--- login.py
+++ login.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 class Login:
     def __init__(self, headless = False):
